042b502401a5f82c49907f7a7719c5a659406f39
Forbes Gray <fnbg75@gmail.com>
2023-08-05 16:49:50 +0100

    Add block to pickture element to prevent image resize flicker

--- pages/[category_slug]/[product_slug].vue
+++ pages/[category_slug]/[product_slug].vue
@@ -2,14 +2,14 @@
   <div class="bg-yellow-100 py-6">
     <div v-if="product" class="container">
       <div class="grid gap-6 lg:grid-cols-2 bg-white">
-        <div class="aspect-square">
+        <div>
           <nuxt-picture
             v-if="product.image"
             :src="product.image.url"
             :alt="product.name"
             sizes="sm:100vw md:50vw lg:600px"
             densities="x1 x2"
-            class="w-full h-full object-cover"
+            class="aspect-square"
           />
         </div>
         <div class="flex flex-col p-6">
